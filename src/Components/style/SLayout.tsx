64b28e562b86acb737fe542cdd722c5a93e94c33
haron <haronxlee@gmail.com>
2023-07-22 03:00:04 +0900

    💄Design: GlobalStyle 컬러 변수height 변수 추가 및  태그초기화, 최상단 layout 스타일 컴포넌트 추가

---
+++ src/Components/style/SLayout.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SLayout = styled.div`
+  max-width: 430px;
+  min-width: 375px;
+  height: calc(var(--vh, 1vh) * 100);
+  margin: 0 auto;
+  padding: 0 24px;
+  box-sizing: border-box;
+  background-color: var(--bg);
+`;
+
+export default SLayout;
