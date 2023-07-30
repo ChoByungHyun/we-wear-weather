32b24ebbed8a5c3b516ddbda9ad585cc8e824bb5
d-charlie-kim <d.charlie.kim@gmail.com>
2023-07-31 01:44:02 +0900

    📂 Rename: Pages의 Home에 있는 말풍선 컴포넌트화 (#8)
    SpeechBubble(말풍선) 전체 컴포넌트화
    통일성을 위해 내부 img 클래스 삭제

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,9 +3,7 @@ import Header from 'Components/common/Header';
 import BottomNav from 'Components/common/BottomNav';
 import ParticulateMatter from 'Components/Home/ParticulateMatter';
 import HourlyForcast from 'Components/Home/HourlyForcast';
-import TodayWeather from 'Components/Home/TodayWeather';
-import speechBubbleTail from 'Assets/speech-bubble-tail.svg';
-import weatherFewClouds from 'Assets/weather-few-clouds.svg';
+import SpeechBubble from 'Components/Home/SpeechBubble';
 
 const Home = () => {
   return (
@@ -13,17 +11,7 @@ const Home = () => {
       <Header />
       <main>
         <SUpdateDate>AM 11:10 업데이트</SUpdateDate>
-        <SSpeechBubble>
-          {/* TODO 날씨 이미지마다 말풍선을 덮는 정도가 달라 이미지 파일 수정 필요 */}
-          <img className='todayWeatherImg' src={weatherFewClouds} alt='today-weather' />
-          {/* NOTE 화면 가로 크기가 변경되면 말풍선 가로 공백이 유동적이어서, 말풍선 꼬리를 따로 아래에 붙이는 방식으로 해결 */}
-          <img className='speechBubbleTail' src={speechBubbleTail} alt=' ' />
-          <TodayWeather>
-          </TodayWeather>
-          <p className='todayComment'>
-          날씨가 흐리고 일교차가 심하네요 <br/>가벼운 겉옷 하나 챙기는건 어떨까요?
-          </p>
-        </SSpeechBubble>
+        <SpeechBubble/>
         <SPMHourlySection>
           <ParticulateMatter />
           <HourlyForcast />
@@ -40,37 +28,6 @@ const SUpdateDate = styled.p`
   color: var(--gray-200);
   font-size: 12px;
   margin: 8px;
-`;
-
-const SSpeechBubble = styled.article`
-  width: calc(100% - 32px);
-  margin-bottom: 100px;
-  background-color: white;
-  border-radius: 10px;
-  position: relative;
-  text-align: center;
-  padding: 0 16px;
-
-  .speechBubbleTail {
-    position: absolute;
-    bottom: -70px;
-    left: calc(50% - 160px);
-  }
-
-  .todayWeatherImg {
-    position: absolute;
-    top: -80px;
-    right: 0;
-  }
-  
-  .todayComment {
-    padding: 24px 0 36px 0;
-    font-size: 16px;
-    color: var(--gray-800);
-    font-weight: 500;
-    border-top: 1px solid var(--gray-200);
-    line-height: 150%;
-  }
 `;
 
 const SPMHourlySection = styled.section`
