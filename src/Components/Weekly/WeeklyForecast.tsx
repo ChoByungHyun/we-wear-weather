6be9256586171118af405da5a8a0ae00edb1469f
haron <haronxlee@gmail.com>
2023-08-09 21:17:39 +0900

    ✨Feat: 주간 예보보 API 데이터 컴포넌트로 뿌려주기 (#30)

--- src/Components/Weekly/WeeklyForecast.tsx
+++ src/Components/Weekly/WeeklyForecast.tsx
@@ -1,21 +1,73 @@
-import React, { FC } from 'react';
-import WeeklyItem from 'Components/Weekly/WeeklyItem';
-import week from 'Mock/weather';
+import React, { FC, useEffect, useState } from 'react';
 import { styled } from 'styled-components';
+import { useQuery } from 'react-query';
+import { useRecoilValue } from 'recoil';
+import WeeklyItem from 'Components/Weekly/WeeklyItem';
+import useOpenWeatherAPI from 'API/useOpenWeatherAPI';
+import { userLocationAtom } from 'Atom/userLocationAtom';
+import formatDateTime from 'Utils/formatDateTime';
+import filterMinMax from 'Utils/filterMinMax';
+
+import { ItemType } from 'types/weeklyType';
 
 const WeeklyForecast: FC = () => {
+  const latLonData = useRecoilValue(userLocationAtom);
+  const { getCityWeather, getForecast } = useOpenWeatherAPI();
+  const [days, setDays] = useState<ItemType[]>([]);
+
+  const cityRes = useQuery('cityWeather', () => getCityWeather(latLonData));
+  const weeklyRes = useQuery('weeklyForecast', () => getForecast(latLonData));
+
+  const today = cityRes?.data;
+
+  // 주간예보 필터링 effect
+  useEffect(() => {
+    if (weeklyRes.data) {
+      const objectData = weeklyRes.data?.list;
+      const dataArray: ItemType[] = Object.values(objectData);
+      const changedDtArr = dataArray.map((item) => {
+        const formattedDate = formatDateTime(item.dt_txt); // 새로운 날짜 포맷
+
+        return {
+          ...item,
+          dt: formattedDate,
+        };
+      });
+
+      const filteredData = filterMinMax(changedDtArr);
+
+      setDays(filteredData);
+    }
+  }, [weeklyRes.data]);
+
+  console.log(days);
+
+  if (weeklyRes.isLoading) {
+    return <p>로딩중...</p>;
+  }
+
+  if (weeklyRes.isError) {
+    return <p>주간예보 날씨가 존재하지 않습니다.</p>;
+  }
+
   return (
     <>
-      <STitle>주간 일기예보</STitle>
+      <STitle>{weeklyRes.data?.city.name} 주간 일기예보</STitle>
       <SLayout>
-        {week.map((day, idx) => (
+        <WeeklyItem
+          day='오늘'
+          min={Math.ceil(today?.main.temp_min) + '°'}
+          max={Math.ceil(today?.main.temp_max) + '°'}
+          temp={Math.ceil(today?.main.temp) + '°'}
+        />
+        {days.map((day, idx) => (
           <WeeklyItem
             key={idx}
             day={day.dt}
-            min={day.main.min}
-            max={day.main.max}
-            temp={day.main.temp}
-            icon={day.main.icon}
+            min={Math.ceil(day.main.temp_min) + '°'}
+            max={Math.ceil(day.main.temp_max) + '°'}
+            temp={Math.ceil(day.main.temp) + '°'}
+            // icon={day.main.icon}
           />
         ))}
       </SLayout>
