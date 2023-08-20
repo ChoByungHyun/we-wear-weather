40dc6e02c3717f668b5700498f1460b5ba47cfe0
haron <haronxlee@gmail.com>
2023-08-20 18:18:48 +0900

    Feat: weekly, hourly 대응 Type 추가

--- src/types/weeklyType.ts
+++ src/types/weeklyType.ts
@@ -42,3 +42,28 @@ export type ItemType = {
   weather: WeatherType[];
   wind: WindType;
 };
+
+// forecast 전체 타입
+export type coordType = {
+  lat: number;
+  lon: number;
+};
+
+export type cityType = {
+  coord: coordType;
+  country: string;
+  id: number;
+  name: string;
+  population: number;
+  sunrise: number;
+  sunset: number;
+  timezone: number;
+};
+
+export type ForecastAllType = {
+  city: cityType;
+  cnt: number;
+  cod: string;
+  list: ItemType[];
+  message: number;
+};
